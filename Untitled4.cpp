30688fae804219508c5a7b2eb7beec918643a895
nduynam128-tech <nduynam128@gmail.com>
2026-05-26 20:13:13 +0700

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+	int A;
+	printf("5: ");
+    scanf("%d", &A);
+    int B;
+    printf("B: ");
+    scanf("%d", &B);
+    if( A>B ){
+		printf("A");
+	}else{
+		
+		printf("B");
+	}
+}
